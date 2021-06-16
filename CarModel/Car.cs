ee0ebf15fe8fa30526b2d0e6a7b3a3879a6dd892
lwnrhf0 <felix.rhomberg@liebherr.com>
2021-06-16 13:54:35 +0200

    Readme changed

---
+++ CarModel/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CarModel
+{
+    public class Car
+    {
+        public string ModelName { get; set; }
+        public string Brand { get; set; }
+        public int MaxSpeed { get; set; }
+        public string Color { get; set; }
+
+        public Car(string brand, string modelname, string color)
+        {
+            this.Brand = brand;
+            this.ModelName = modelname;
+            this.Color = color;
+        }
+
+        public int GetMaxSpeed()
+        {
+            if(this.Color == "red")
+            {
+                this.MaxSpeed = 250;
+            }
+            else
+            {
+                this.MaxSpeed = 150;
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![Version: 1.0 Release](https://img.shields.io/badge/version-1.0%20Release-green.svg)](https://github.com/FelixRho/CarTestRepo)   [![Version](https://travis-ci.com/FelixRho/pipeline.svg?branch=main)](https://travis-ci.com/github/FelixRho/CarTestRepo)   [![codecov](https://codecov.io/gh/FelixRho/pipeline/branch/main/graph/badge.svg?token=KF2BQCUQ8C)](https://codecov.io/gh/FelixRho/CarTestRepo)   [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+
+
 # CarTestRepo
 A testrepository with a simple car model
